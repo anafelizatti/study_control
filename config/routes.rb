7607998360ce78b73152176979e303f9f9d1cc6a
anafelizatti <apfelizatti@gmail.com>
2020-12-09 13:45:22 -0300

    Menu de opções + rout config

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
+  root to: redirect('/study_items')
   resources :study_items
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
