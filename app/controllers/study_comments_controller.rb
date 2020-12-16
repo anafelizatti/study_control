92b2ebb0ef2b1bcb515ba102cd5b34adf3545bdc
anafelizatti <apfelizatti@gmail.com>
2020-12-16 13:39:17 -0300

    refatorado, destroy

--- app/controllers/study_comments_controller.rb
+++ app/controllers/study_comments_controller.rb
@@ -6,6 +6,14 @@ class StudyCommentsController < ApplicationController
     redirect_to study_item_path(@study_item)
   end
     
+
+  def destroy
+    @study_item = StudyItem.find(params[:study_item_id])
+    @study_comment = @study_item.study_comments.find(params[:id])
+    @study_comment.destroy
+    redirect_to study_item_path(@study_item)
+  end
+
   private
     def study_comment_params
       params.require(:study_comment).permit(:commenter, :body)
--- app/models/study_item.rb
+++ app/models/study_item.rb
@@ -1,4 +1,4 @@
 class StudyItem < ApplicationRecord
-    has_many :study_comments
+    has_many :study_comments, dependent: :destroy
     validates :title, presence: { message: 'Não pode estar em branco.'}
 end
